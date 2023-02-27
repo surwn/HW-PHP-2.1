33a0d7ef28a9ed672ea2dfdd643fa8cab3278752
Ekarerina <ekaterinashuvalova19801309@gmail.com>
2023-02-27 23:37:33 +0300

    header+main

--- index.php
+++ index.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/normalize.css" rel="stylesheet">
     <title>PHP - Hello, World!</title>
 </head>
 <body>
 <header class="">
-  <img src="../img/" alt="logo_poli" class="logo">
-  <div class="flex-block">
-    <a href="#" class="about">
-      
+    <div class="flex-block">
+    <img src="../img/maxresdefault.jpg" alt="logo_poli" class="logo">
+  
+    <a href="#" class="about" style="margin-left: 226px;">
+    2.1.Домашняя работа: Hello, World!
     </a>
+    </div> 
 </header>
         <h1><?php echo 'Hello, World!'; ?></h1>
 </body>
---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+<header class="">
+  <img src="../img/" alt="logo_poli" class="logo">
+  <div class="flex-block">
+    <a href="#" class="about">
+      
+    </a>
+</header>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+</body>
+</html>
